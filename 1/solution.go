fd7fbf036d7e641e32ba72ea74b185b307981ded
Dean Milojevic <milojevicdean@gmail.com>
2021-01-22 14:46:36 +0100

    Solution for the Day #1 Part 2

--- 1/solution.go
+++ 1/solution.go
@@ -1,8 +1,12 @@
 package main
 
+import (
+	"sort"
+)
+
 func main() {}
 
-func solution(input []int) int {
+func solutionPartOne(input []int) int {
 	target := 2020
 
 	m := make(map[int]bool)
@@ -11,8 +15,28 @@ func solution(input []int) int {
 
 		if m[tmp] {
 			return num * tmp
-		} else {
-			m[num] = true
+		}
+
+		m[num] = true
+	}
+
+	return 0
+}
+
+func solutionPartTwo(input []int) int {
+	sort.Ints(input)
+	length := len(input)
+	for i := 0; i < length-2; i++ {
+		j, k := i+1, length-1
+		for j < k {
+			cur := input[i] + input[j] + input[k]
+			if cur > 2020 {
+				k--
+			} else if cur < 2020 {
+				j++
+			} else {
+				return input[i] * input[j] * input[k]
+			}
 		}
 	}
 
--- 1/solution_test.go
+++ 1/solution_test.go
@@ -10,7 +10,7 @@ import (
 func Test01(t *testing.T) {
 	input := []int{1721, 979, 366, 299, 675, 1456}
 
-	result := solution(input)
+	result := solutionPartOne(input)
 
 	if result != 514579 {
 		t.Fail()
@@ -35,9 +35,34 @@ func Test02(t *testing.T) {
 		input = append(input, i)
 	}
 
-	result := solution(input)
+	result := solutionPartOne(input)
 
 	if result != 898299 {
+		t.Fail()
+	}
+}
+
+func Test03(t *testing.T) {
+	f, err := os.Open("input.txt")
+
+	if err != nil {
+		t.Fail()
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	input := make([]int, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		i, _ := strconv.Atoi(line)
+		input = append(input, i)
+	}
+
+	result := solutionPartTwo(input)
+
+	if result != 143933922 {
 		t.Fail()
 	}
 }

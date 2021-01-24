5f32b7ca6a170283afb1374efd333b1f33538ec0
Dean Milojevic <milojevicdean@gmail.com>
2021-01-24 20:56:29 +0100

    Solution for the Day #6 Part 2

--- 6/solution_test.go
+++ 6/solution_test.go
@@ -73,3 +73,56 @@ func Test04(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func Test03(t *testing.T) {
+	input := []string{
+		"abc",
+		"",
+		"a",
+		"b",
+		"c",
+		"",
+		"ab",
+		"ac",
+		"",
+		"a",
+		"a",
+		"a",
+		"a",
+		"",
+		"b",
+		"",
+	}
+
+	result := solutionTwo(input)
+
+	if result != 6 {
+		t.Fail()
+	}
+}
+
+func Test05(t *testing.T) {
+	f, err := os.Open("input.txt")
+
+	if err != nil {
+		t.Fail()
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	input := make([]string, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		input = append(input, line)
+	}
+
+	input = append(input, "")
+
+	result := solutionTwo(input)
+
+	if result != 3398 {
+		t.Fail()
+	}
+}
--- 6/solution_two.go
+++ 6/solution_two.go
@@ -1,6 +1,38 @@
 package main
 
 func solutionTwo(input []string) int {
+	empty := ""
+	total := 0
 
-	return 0
+	groupSize := 0
+	questions := map[byte]int{}
+	answers := 0
+	for _, line := range input {
+		if line == empty {
+			for _, val := range questions {
+				if val == groupSize {
+					total++
+				}
+			}
+
+			groupSize = 0
+			questions = map[byte]int{}
+			answers = 0
+			continue
+		}
+
+		groupSize++
+
+		for i := 0; i < len(line); i++ {
+			val, ok := questions[line[i]]
+			if ok {
+				questions[line[i]] = val + 1
+			} else {
+				questions[line[i]] = 1
+				answers++
+			}
+		}
+	}
+
+	return total
 }
